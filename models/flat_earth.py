c70217f3e791b9fa46fb13d76eef7072576086ff
Przemek Lekston <p.lekston@gmail.com>
2017-05-05 00:38:43 +0200

    Added 'System' class to handle integration and Jacobian for kinematic equations.

--- models/flat_earth.py
+++ models/flat_earth.py
@@ -9,7 +9,16 @@ g = 9.81
     - wind_dir - where to the wind is blowing
 """
 
-def kin_equats():
+
+def kin_equats(time, state, TAS, wind):
+    # TODO must provide 'p' - either as a state or control variable
+    #   It is simpler to have it as control variable and keeps
+    #   its state separately
+
+    phi = state[3]
+    psi = state[4]
+
+    wind_dir, wind_spd = wind
 
     dx_dt = cos(psi) * TAS + cos(wind_dir) * wind_spd
 
@@ -20,3 +29,17 @@ def kin_equats():
     dpsi_dt = g * tan(phi) / TAS
 
     return np.array([dx_dt, dy_dt, dphi_dt, dpsi_dt])
+
+
+def kin_equats_jac(time, state, TAS):
+
+    phi = state[3]
+    psi = state[4]
+
+    jac = np.zeros([4,4])
+
+    jac[0,3] = -sin(psi) * TAS
+    jac[1,3] = cos(psi) * TAS
+    jac[3,2] = g / (cos(phi)**2 * TAS)
+
+    return jac
