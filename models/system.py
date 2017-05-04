91b0f0776fbd42d4fde3ad667b0f4e8e3bcf995e
Przemek Lekston <plekston@flytechuav.com>
2017-05-05 00:38:43 +0200

    Added 'System' class to handle integration and Jacobian for kinematic equations.

---
+++ models/system.py
@@ -0,0 +1,47 @@
+import numpy as np
+from scipy.integrate import ode
+from models.flat_earth import kin_equats, kin_equats_jac
+
+
+class System(object):
+
+    def __init__(self, phi=0, psi=0, TAS=15,
+                 integrator='dopri5', use_jac=False, **integrator_params):
+
+        self.x = 0
+        self.y = 0
+        self._state = np.array([x, y, phi, psi])
+        self.TAS = TAS
+
+        self.kin_equats = kin_equats
+
+        if use_jac:
+            jac_kin = kin_equats_jac
+        else:
+            jac_kin = None
+
+        self._ode_kin_equats = ode(kin_equats, jac=jac_kin)
+        self._ode_kin_equats.set_integrator(integrator, **integrator_params)
+        self._ode_kin_equats.set_initial_value(y=self._state)
+
+    @property
+    def state(self):
+        return self._state
+
+    def propagate(self, env, dt=0.01):
+
+        TAS   = self.TAS
+        wind  = env.wind
+
+        time = self._ode_kin_equats.t + dt
+
+        self._ode_kin_equats.set_f_params(TAS, wind)
+        state = self._ode_kin_equats.integrate(time)
+
+        if self._ode_kin_equats.successful():
+            self._state = state
+        else:
+            raise RuntimeError('Integration was not successful')
+
+        return self._state
+
