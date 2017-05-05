bae695cc5897bb76a69ca7eb994eb83077305400
Przemek Lekston <p.lekston@gmail.com>
2017-05-05 13:05:23 +0200

    Refactor TAS naming and correct whitespace errors.

--- models/flat_earth.py
+++ models/flat_earth.py
@@ -10,7 +10,7 @@ g = 9.81
 """
 
 
-def kin_equats(time, state, TAS, wind):
+def kin_equats(time, state, tas, wind):
     # TODO must provide 'p' - either as a state or control variable
     #   It is simpler to have it as control variable and keeps
     #   its state separately
@@ -20,26 +20,25 @@ def kin_equats(time, state, TAS, wind):
 
     wind_dir, wind_spd = wind
 
-    dx_dt = cos(psi) * TAS + cos(wind_dir) * wind_spd
+    dx_dt = cos(psi) * tas + cos(wind_dir) * wind_spd
 
-    dy_dt = sin(psi) * TAS + sin(wind_dir) * wind_spd
+    dy_dt = sin(psi) * tas + sin(wind_dir) * wind_spd
 
-    dphi_dt = p # TODO must have low level roll rate controller
+    dphi_dt = p  # TODO must have low level roll rate regulator
 
-    dpsi_dt = g * tan(phi) / TAS
+    dpsi_dt = g * tan(phi) / tas
 
     return np.array([dx_dt, dy_dt, dphi_dt, dpsi_dt])
 
-
-def kin_equats_jac(time, state, TAS):
+def kin_equats_jac(time, state, tas):
 
     phi = state[3]
     psi = state[4]
 
-    jac = np.zeros([4,4])
+    jac = np.zeros([4, 4])
 
-    jac[0,3] = -sin(psi) * TAS
-    jac[1,3] = cos(psi) * TAS
-    jac[3,2] = g / (cos(phi)**2 * TAS)
+    jac[0, 3] = -sin(psi) * tas
+    jac[1, 3] = cos(psi) * tas
+    jac[3, 2] = g / (cos(phi)**2 * tas)
 
     return jac
