9ae0b4addfb06d2b202b62ecf4a5104b544d5d86
Przemek Lekston <p.lekston@gmail.com>
2017-05-05 20:25:23 +0200

    Extend aircraft component update methods

--- aircraft/aircraft.py
+++ aircraft/aircraft.py
@@ -1,18 +1,30 @@
 from regulator import rollReg, navReg
+from flightPlan import basic_fpl
 
 
 class Aircraft(object):
 
-    def __init__(self):
+    def __init__(self, flightPlan=basic_fpl):
         self._TAS = 15  # m/s
-        self._regulators = dict(roll=rollReg,
-                                nav=navReg)
+        self._fpl = flightPlan
+        self._regulators = dict(roll=rollReg, nav=navReg)
 
     @property
     def airspeed(self):
         return self._TAS
 
-    def update(self, system, demand):
-        for key in self._regulators:
-            self._regulators[key].update(system, demand)
+    def update(self, system):
+
+        # pass position to fpl object to verify reaching waypoints
+        cur_leg = self._fpl.update(system.state[:2])
+
+        roll_dem      = self._regulators['nav'].update(system, cur_leg)
+        roll_rate_dem = self._regulators['roll'].update(system, roll_dem)
+
+        # TODO logging of aircraft control variables
+
+        return roll_rate_dem
+
+basic_aircraft = Aircraft()
+
 
