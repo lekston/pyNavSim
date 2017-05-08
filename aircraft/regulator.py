8108d3642f14d5a46e6d5990e0ac9dc36de8bb71
Przemek Lekston <p.lekston@gmail.com>
2017-05-08 09:37:08 +0200

    Several regulator enhancements:   - Roll: Correct roll demand rate limiting   - Roll: Initialize LP filter states   - L1: Implement navigation to start of the leg if behind

--- aircraft/regulator.py
+++ aircraft/regulator.py
@@ -2,6 +2,7 @@ from abc import abstractmethod
 import numpy as np
 import utils.basic_nav as bn
 
+
 class Regulator(object):
 
     @abstractmethod
@@ -24,18 +25,20 @@ class Regulator(object):
 
 class RollRegulator(Regulator):
 
-    def __init__(self, phi_max=35, p_max=80, p_dot_max=80):
+    def __init__(self, phi_max=35, p_max=60, p_dot_max=80):
         super(RollRegulator, self).__init__()
-        self._phi_max    = np.deg2rad(phi_max)      # 40 deg by default
+        self._filt_init         = False
+        self._prev_phi_dem      = 0
+        self._phi_dem_dot_max   = np.deg2rad(30)    # 30 deg/sec by default
+        self._phi_max    = np.deg2rad(phi_max)      # 35 deg by default
         self._p_max      = np.deg2rad(p_max)        # 60 deg/sec by default
         self._p_dot_max  = np.deg2rad(p_dot_max)    # 80 deg/sec^2 by default
         self._prev_err   = 0
         self._prev_state = 0
-        self._prev_p_cmd = 0
-        self._k_p        = 0.5
+        self._k_p        = 0.1
         self._k_d        = 3
-        self._par_list   = ['p_dot_dem']
-        self._log_dict   = {'p_dot_dem': 0}
+        self._log_dict   = {'p_dem': 0, 'phi_dem_ll': 0}
+        self._par_list   = [key for key in self._log_dict.iterkeys()]
 
     @property
     def phi_max(self):
@@ -51,23 +54,26 @@ class RollRegulator(Regulator):
 
     def update(self, system, dt, demand, observables):
 
-        # TODO consider time scale of the regulator
-
         roll = system.state_dict['phi']
+
+        if not self._filt_init:
+            self._filt_init = True
+            self._prev_phi_dem = roll   # forces initial error to be 0
+
         roll_dem = np.clip(demand, -self._phi_max, self._phi_max)
-        # TODO rate-limit the roll demand
+        # rate-limit roll demand
+        roll_dem = np.clip(roll_dem, self._prev_phi_dem - self._phi_dem_dot_max * dt,
+                                     self._prev_phi_dem + self._phi_dem_dot_max * dt)
         err = roll_dem - roll
-
-        err = np.clip(err, self._prev_err - self._p_max*dt,
-                           self._prev_err + self._p_max*dt)
-
         err_dot_raw = (self._prev_err - err)/dt
         err_dot_flt = 0.8 * self._prev_state + 0.2 * err_dot_raw
 
         roll_rate_cmd = self._k_p * err - self._k_d * err_dot_flt
 
-        self._log_dict['p_dot_dem'] = roll_rate_cmd
+        self._log_dict['p_dem'] = roll_rate_cmd
+        self._log_dict['phi_dem_ll'] = roll_dem
 
+        self._prev_phi_dem = roll_dem
         self._prev_state = err_dot_flt
         self._prev_err   = err
         return roll_rate_cmd
@@ -112,7 +118,7 @@ class L1NavRegulator(Regulator):
         wind_vec = np.array([np.sin(wind[0]), np.cos(wind[0])]) * wind[1]
         tas_vec = np.array([np.sin(psi), np.cos(psi)]) * tas
         current_loc_vec = np.array([system.state_dict['x'],
-                                     system.state_dict['y']])
+                                    system.state_dict['y']])
 
         K_L1 = 4.0 * self.L1_damping**2
         L1_dist = 1/np.pi**2 * self.L1_damping * self.L1_period * gnd_spd
@@ -129,13 +135,16 @@ class L1NavRegulator(Regulator):
 
         coveredTrack_dist = np.dot(origin_vec, or2target_vec)   # alongTrackDist
 
-        if (origin_dist > L1_dist) and (coveredTrack_dist/np.max([origin_dist, 1.]) < np.sqrt(2)/2.):
+        if (origin_dist > L1_dist) and (coveredTrack_dist/np.max([origin_dist, 1.]) < -np.sqrt(2)/2.):
             # calculate Nu to fly to WP A
-            Nu = 0
-            nav_bearing = 0
+            origin_vec *= 1./bn.norm_2d(origin_vec)             # normalize
+            xtrackVel = -np.cross(gnd_spd_vect, -origin_vec)    # gnd_spd % A_air_norm (minus due to x-y coord swap)
+            ltrackVel = np.dot(gnd_spd_vect, -origin_vec)
+            Nu = np.arctan2(xtrackVel, ltrackVel)
+            nav_bearing = np.arctan2(origin_vec[0], origin_vec[1])
         else:
             # calculate Nu to fly along the AB line
-            xtrackVel = -np.cross(gnd_spd_vect, or2target_vec)   # gnd_spd % AB_norm (minus due to x-y coord swap)
+            xtrackVel = -np.cross(gnd_spd_vect, or2target_vec)  # gnd_spd % AB_norm (minus due to x-y coord swap)
             ltrackVel = np.dot(gnd_spd_vect, or2target_vec)
             Nu2 = np.arctan2(xtrackVel, ltrackVel)              # angle of velocity relative to track line
 
@@ -144,7 +153,7 @@ class L1NavRegulator(Regulator):
             Nu1 = np.arcsin(sine_Nu1)
 
             Nu = Nu1 + Nu2
-            nav_bearing = bn.get_bearing(prev_wp, next_wp) + Nu1;
+            nav_bearing = bn.get_bearing(prev_wp, next_wp) + Nu1
 
         Nu = np.clip(Nu, -np.pi/2, np.pi/2)
 
