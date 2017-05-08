f90e53dc0c4fae4d0a2b56435927823e43a454fd
Przemek Lekston <plekston@flytechuav.com>
2017-05-08 10:05:58 +0200

    [Ex] Adjust examples to use new plot scaling routine.

--- examples/ex_0.py
+++ examples/ex_0.py
@@ -4,19 +4,12 @@ try:
     from aircraft.aircraft import basic_aircraft
     from models.system import System
     from models.env import Env
+    from utils import drawing as dr
     from sim import Sim
 except:
     print "Check run path, must be run from: ./40.SmoothTraj"
 
 import matplotlib.pyplot as plt
-
-
-def set_1to1_scale(ax):
-    x_min, x_max = ax.get_xlim()
-    y_min, y_max = ax.get_ylim()
-
-    ax.set_xlim(min(x_min,y_min), max(x_max, y_max))
-    ax.set_ylim(min(x_min,y_min), max(x_max, y_max))
 
 
 phi_init = 0.0
@@ -38,17 +31,17 @@ fig1 = plt.figure(1)
 plt.plot(sim.logs['x'][:-2], sim.logs['y'][:-2])
 # plt.plot(time[:-2], sim.logs['x'][:-2])
 ax1 = fig1.gca()
-set_1to1_scale(ax1)
+dr.set_1to1_scale(ax1)
 
 # TODO fig2 & fig3 to subplots
 fig2 = plt.figure(2)
-plt.plot(time, sim.logs['p'])
+plt.plot(time, sim.logs['p'], 'b-', time, sim.logs['p_dem'], 'r-')
 ax2 = fig2.gca()
 # set_1to1_scale(ax2)
 ax2.set_ylabel('p[rad/s]')
 
 fig3 = plt.figure(3)
-plt.plot(time, sim.logs['phi'])
+plt.plot(time, sim.logs['phi'], 'b-', time, sim.logs['phi_dem_ll'], 'r-')
 ax3 = fig3.gca()
 # set_1to1_scale(ax3)
 ax3.set_ylabel('phi[rad]')
